18f9c76ef865d94f130f17225e7f7c773bb20a73
Luis Hernandez <luishernandez8195@gmail.com>
2022-09-28 08:22:25 -0700

    Versión inicial del proyecto

---
+++ src/mx/itson/pastor/persistencia/Conexion.java
@@ -0,0 +1,42 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.itson.pastor.persistencia;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+/*
+ * 
+ *
+ * @author lahg2
+ */
+public class Conexion {
+   
+    public static Connection obtener(){
+       Connection conexion = null;
+       try{
+          conexion = DriverManager.getConnection("jdbc:mysql://localhost/pastordb?user=root&password=");
+       }catch(Exception e){
+           System.err.print(e.getMessage());
+       }
+       return conexion;
+    }
+    
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
